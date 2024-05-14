4802d01911880311989038c0ccfbc940f71e5db5
gustadev <gustavodevmail@gmail.com>
2024-05-14 18:32:08 -0300

    Criação do metodo Post para teste do codigo.

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/recebe")
+def recebe(dados: dict):
+    # Processa os dados recebidos (você pode adicionar sua lógica aqui)
+    resultado = "Dados processados com sucesso!"
+    
+    # Registra o resultado no log
+    print(f"Resultado: {resultado}")
+    
+    return {"message": resultado}
